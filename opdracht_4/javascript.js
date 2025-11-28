af4ce74d7c1ad188b495f14d421aab380aceb3c6
JackLee9028664 <9028664@student.zadkine.nl>
2025-11-28 14:24:40 +0100

    Add files via upload

---
+++ opdracht_4/javascript.js
@@ -0,0 +1,7 @@
+prompt("in welk jaar ben je geboren is");
+
+let naam = prompt("in welk jaar ben je geboren");
+
+const geboortejaar = 2025 - prompt("in welk jaar ben je geboren");
+
+document.getElementById("jaar").innerHTML = ("jij bent ongeveer "  + geboortejaar  + " jaar oud.")
