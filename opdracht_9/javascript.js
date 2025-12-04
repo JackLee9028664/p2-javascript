84f27516a9cd8d61a06abb13159d29ea914a36b6
JackLee9028664 <9028664@student.zadkine.nl>
2025-12-04 13:05:55 +0100

    Add files via upload

---
+++ opdracht_9/javascript.js
@@ -0,0 +1,7 @@
+function verberg_tekst() {
+    document.getElementById("tekst").style.display = "none";
+}
+
+function toon_tekst() {
+    document.getElementById("tekst").style.display = "block";
+}
