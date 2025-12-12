8b8983dbd2dc66d369966ab96e7db9aefd686e26
JackLee9028664 <9028664@student.zadkine.nl>
2025-12-12 11:02:54 +0100

    Add files via upload

---
+++ opdracht_13/javascript.js
@@ -0,0 +1,11 @@
+
+ let leeftijd = 18;
+ let product = prompt("wat is je leeftijd?");
+ document.getElementById("1C")
+
+if ( product <= leeftijd) {
+document.getElementById("1C").innerHTML = ("Je mag hier nog niet naar binnen");
+} else {
+document.getElementById("1C").innerHTML = ("Welkom!");
+}
+
