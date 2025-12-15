ccccd44ace9c1a5f24804a0de788b16bf8edf76c
JackLee9028664 <9028664@student.zadkine.nl>
2025-12-15 15:31:36 +0100

    Add files via upload

---
+++ opdracht_14/javascript.js
@@ -0,0 +1,18 @@
+let drinken = prompt("Wat wil je drinken? (cola, water of koffie)");
+
+ if(drinken === "cola") {
+    document.getElementById("drank").innerHTML = "Je krijgt een blikje cola 🥤"
+ } else {
+   
+
+ }   if(drinken === "water") {
+    document.getElementById("drank").innerHTML = "Goed bezig, lekker gezond 💧";
+ } else {
+   
+ }
+
+ if(drinken === "koffie") {
+    document.getElementById("drank").innerHTML = "Energie incoming ☕";
+ } else {
+
+ }
