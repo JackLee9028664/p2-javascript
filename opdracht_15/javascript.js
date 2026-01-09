f524a2df9f3f41fefdd818d9b11fee674220efbf
JackLee9028664 <9028664@student.zadkine.nl>
2026-01-09 09:18:50 +0100

    Add files via upload

---
+++ opdracht_15/javascript.js
@@ -0,0 +1,15 @@
+ let teller = 0
+ let getal = 3;
+
+
+
+function klik() {
+teller = teller + 1;
+
+if(teller => getal) {
+  document.getElementById("1c").innerHTML = "Dat zijn er best veel…";
+} else {
+document.getElementById("1c").innerHTML = "je hebt geen redbull";
+}
+}
+
