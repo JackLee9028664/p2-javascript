6ac5a692dca9a06872dbf9924e35fb60d5f0bd14
JackLee9028664 <9028664@student.zadkine.nl>
2026-01-27 13:59:18 +0100

    Add files via upload

---
+++ opdracht_25/javascript.js
@@ -0,0 +1,10 @@
+myInterval = setInterval(setColor, 500);
+ 
+function setColor() {
+  let x = document.body;
+  x.style.backgroundColor = x.style.backgroundColor == "blue" ? "pink" : "blue";
+}
+ 
+function stopColor() {
+  clearInterval(myInterval);
+}
